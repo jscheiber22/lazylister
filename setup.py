6534b794c21aa895747a894cd3eccf089d0cf07f
James Scheiber <jscheiber22@gmail.com>
2020-11-13 14:45:25 -0800

    minor updates to setup.py to be cooler

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='lazylister',
-    version='1.0.0',
+    version='1.0.1',
+    license='MIT',
     description='A cute little lister script that literally just returns a list of each line in a file.',
     author='James Scheiber',
     author_email='jscheiber22@gmail.com',
+    url="https://github.com/jscheiber22/lazylister",
     packages=find_packages(include=['lazylister', 'lazylister.*'])
 )
